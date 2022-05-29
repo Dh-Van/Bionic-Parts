a77394676020194d0088dd173d0312ba9a8f299d
Dh-Van <dhvan17shah@gmail.com>
2022-05-29 23:47:39 +0000

    init commit

---
+++ bionic-parts/resources/js/main.js
@@ -0,0 +1,54 @@
+const table = window.document.getElementById("table");
+const json = JSON.stringify('/data.json');
+
+console.log(json);
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+if(dataMatrix === undefined)
+    var dataMatrix = [[], [], [], [], []];
+
+for(r = 0; r < dataMatrix[0].length; r++){
+    addAssembly(dataMatrix[r]);
+}
+
+function addAssembly(dataArray){
+    var row = table.insertRow(-1);
+    var partNumberCell = row.insertCell(0);
+    var typeCell = row.insertCell(1);
+    var nameCell = row.insertCell(2);
+    var parentCell = row.insertCell(3);
+    var statusCell = row.insertCell(4);
+
+    partNumberCell.innerHTML = dataArray[0];
+    typeCell.innerHTML = dataArray[1];
+    nameCell.innerHTML = dataArray[2];
+    parentCell.innerHTML = dataArray[3];
+    statusCell.innerHTML = dataArray[4];
+
+
+    // Adds data to the data matrix
+    dataMatrix[0].push(partNumberCell.innerHTML);
+    dataMatrix[1].push(typeCell.innerHTML);
+    dataMatrix[2].push(nameCell.innerHTML);
+    dataMatrix[3].push(parentCell.innerHTML);
+    dataMatrix[4].push(statusCell.innerHTML);
+
+    // console.log(dataMatrix[0]);
+
+    console.log(`${dataMatrix[0]}`);
+    console.log(`${dataMatrix[1]}`);
+    console.log(`${dataMatrix[2]}`);
+    console.log(`${dataMatrix[3]}`);
+    console.log(`${dataMatrix[4]}`);
+}
---
+++ sandbox/resources/js/main.js
@@ -0,0 +1,40 @@
+var isVisible = false;
+var firstInput = true;
+
+document.getElementById("window").addEventListener('submit', function(e){e.preventDefault(); replaceText();}, false);
+
+function popped(){
+    let window = document.getElementById("window");
+    if(!isVisible){
+        window.style.visibility = "visible";
+        isVisible = true;
+    } else {
+        window.style.visibility = "hidden";
+        isVisible = false;
+    }
+}
+
+function replaceText(){
+
+    let form = document.getElementById("form");
+
+    let inputs = document.querySelectorAll("#test");
+
+    let text = "";
+
+    for(i = 0; i < inputs.length; i++){
+        text += inputs[i].value;
+    }
+
+    if(firstInput){
+        document.getElementById("display").innerHTML = text;
+        firstInput = !firstInput;
+    } else
+    document.getElementById("display").innerHTML += "<br>" + text;
+
+
+    for(i = 0; i < inputs.length; i++){
+        inputs[i].value = '';
+    }
+    
+}
