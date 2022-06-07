a9803b591fb636158fd0d803c1b4de9824411ba8
Dh-Van <dhvan17shah@gmail.com>
2022-06-07 18:01:59 +0000

    fixed colors and stuff

--- node-bionic-parts/public/resources/js/form.js
+++ node-bionic-parts/public/resources/js/form.js
@@ -3,6 +3,9 @@ const popup = document.getElementById("popup");
 // The background when the popup is displayed
 const overlay = document.getElementById("overlay");
 
+const LIGHT_GREEN = "#c5edc7" ;
+const TRANSPARENT_RED = "rgba(255, 0, 0, .5)"
+
 // Columns of the form
 const c1 = document.getElementById("c1")
 const c2 = document.getElementById("c2")
@@ -15,11 +18,11 @@ let isVisible = false;
 
 // EventListeners to change the color of the form fields depending on if it is a valid input or not
 {
-c1.addEventListener("input", e => {c1.style.backgroundColor = "#c5edc7"; if(c1.value.length < 1) c1.style.backgroundColor = "rgba(255, 0, 0, .5)";});
-c2.addEventListener("input", e => {c2.style.backgroundColor = "#c5edc7"; if(c2.value.length < 1 || c2.value == "N/A") c2.style.backgroundColor = "rgba(255, 0, 0, .5)";});
-c3.addEventListener("input", e => {c3.style.backgroundColor = "#c5edc7"; if(c3.value.length < 1) c3.style.backgroundColor = "rgba(255, 0, 0, .5)";});
-c4.addEventListener("input", e => {c4.style.backgroundColor = "#c5edc7"; if(c4.value.length < 1) c4.style.backgroundColor = "rgba(255, 0, 0, .5)";});
-c5.addEventListener("input", e => {c5.style.backgroundColor = "#c5edc7"; if(c5.value.length < 1 || c5.value == "N/A") c5.style.backgroundColor = "rgba(255, 0, 0, .5)";});
+c1.addEventListener("input", e => {c1.style.backgroundColor = LIGHT_GREEN; if(c1.value.length < 1) c1.style.backgroundColor = TRANSPARENT_RED;});
+c2.addEventListener("input", e => {c2.style.backgroundColor = LIGHT_GREEN; if(c2.value.length < 1 || c2.value == "N/A") c2.style.backgroundColor = TRANSPARENT_RED;});
+c3.addEventListener("input", e => {c3.style.backgroundColor = LIGHT_GREEN; if(c3.value.length < 1) c3.style.backgroundColor = TRANSPARENT_RED;});
+c4.addEventListener("input", e => {c4.style.backgroundColor = LIGHT_GREEN; if(c4.value.length < 1) c4.style.backgroundColor = TRANSPARENT_RED;});
+c5.addEventListener("input", e => {c5.style.backgroundColor = LIGHT_GREEN; if(c5.value.length < 1 || c5.value == "N/A") c5.style.backgroundColor = TRANSPARENT_RED;});
 }
 
 function formCancel(){
@@ -30,13 +33,7 @@ function formCancel(){
 
 function formSubmit(){
     if(
-        c1.value.length < 1 ||
-        c2.value.length < 1 ||
-        c3.value.length < 1 ||
-        c4.value.length < 1 ||
-        c5.value.length < 1 ||
-        c2.value == "N/A" ||
-        c5.value == "N/A"
+
         ){
             alert("Fill in the required fields");
     } else {
@@ -47,12 +44,6 @@ function formSubmit(){
 }
 
 function popped(){
-    c1.style.backgroundColor = "rgba(255, 0, 0, .5)";
-    c2.style.backgroundColor = "rgba(255, 0, 0, .5)";
-    c3.style.backgroundColor = "rgba(255, 0, 0, .5)";
-    c4.style.backgroundColor = "rgba(255, 0, 0, .5)";
-    c5.style.backgroundColor = "rgba(255, 0, 0, .5)";
-    
     if(!isVisible) {popup.style.display = "initial"; overlay.style.display = "initial";}
     else if(isVisible) {popup.style.display = "none"; overlay.style.display = "none";}
 
